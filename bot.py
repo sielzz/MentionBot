0f5d02ddc9da26368c1235a50f50470bc51f17e5
sielzz <93588547+sielzz@users.noreply.github.com>
2021-11-15 21:29:08 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,20 +16,20 @@ client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
 
 @client.on(events.NewMessage(pattern="^/start$"))
 async def start(event):
-  await event.reply("__**I'm MentionAll Bot**, I can mention almost all members in group or channel 👻\nClick **/help** for more information__\n\n Follow [@AnjanaMadu](https://github.com/AnjanaMadu) on Github",
+  await event.reply("__**Halo Yatim Saya Mention Bot Sielzz**, Saya dapat menyebutkan hampir semua anggota di grup atau saluran 👻\nPencet **/help** biar lu ga bego bego amat__\n\n Tuan Gua [Sielzz](https://t.me/arsilaf) Terganteng di Telegram!!!",
                     buttons=(
-                      [Button.url('📣 Channel', 'https://t.me/harp_tech'),
-                      Button.url('📦 Source', 'https://github.com/AnjanaMadu/MentionAllBot')]
+                      [Button.url('📣 Channel', 'https://t.me/sielzzproject'),
+                      Button.url('🗣️ Tuan Gua', 'https://t.me/arsilaf')]
                     ),
                     link_preview=False
                    )
 @client.on(events.NewMessage(pattern="^/help$"))
 async def help(event):
-  helptext = "**Help Menu of MentionAllBot**\n\nCommand: /mentionall\n__You can use this command with text what you want to mention others.__\n`Example: /mentionall Good Morning!`\n__You can you this command as a reply to any message. Bot will tag users to that replied messsage__.\n\nFollow [@AnjanaMadu](https://github.com/AnjanaMadu) on Github"
+  helptext = "**Menu Bantuan dari Mention Bot Sielzz**\n\nPerintah: /mentionall\n__Lu bisa gunain perintah ini dengan teks apa yang ingin lu sebutkan orang lain.__\n`Example: /mentionall Halo Ngentot!`\n__Lu bisa memberikan perintah ini sebagai balasan untuk pesan apa pun. Bot akan menandai pengguna ke pesan balasan itu__.\n\n Tuan Gua [Sielzz](https://t.me/arsilaf) Terganteng di Telegram!!!"
   await event.reply(helptext,
                     buttons=(
                       [Button.url('📣 Channel', 'https://t.me/harp_tech'),
-                      Button.url('📦 Source', 'https://github.com/AnjanaMadu/MentionAllBot')]
+                      Button.url('🗣️ Tuan Gua', 'https://t.me/arsilaf')]
                     ),
                     link_preview=False
                    )
@@ -37,13 +37,13 @@ async def help(event):
 @client.on(events.NewMessage(pattern="^/mentionall ?(.*)"))
 async def mentionall(event):
   if event.is_private:
-    return await event.respond("__This command can be use in groups and channels!__")
+    return await event.respond("__Perintah ini dapat digunakan dalam grup dan saluran!__")
   
   admins = []
   async for admin in client.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
     admins.append(admin.id)
   if not event.sender_id in admins:
-    return await event.respond("__Only admins can mention all!__")
+    return await event.respond("__Cuma admin yang bisa tolol!__")
   
   if event.pattern_match.group(1):
     mode = "text_on_cmd"
@@ -52,11 +52,11 @@ async def mentionall(event):
     mode = "text_on_reply"
     msg = event.reply_to_msg_id
     if msg == None:
-        return await event.respond("__I can't mention members for older messages! (messages which are sent before I'm added to group)__")
+        return await event.respond("__Gua gabisa tag semua babu lu pada pesan lama! (karena pesan lu udah lama dan lu baru nambahin gua)__")
   elif event.pattern_match.group(1) and event.reply_to_msg_id:
-    return await event.respond("__Give me one argument!__")
+    return await event.respond("__Kasi gua argumen anjing!__")
   else:
-    return await event.respond("__Reply to a message or give me some text to mention others!__")
+    return await event.respond("__reply pesan nya tolol kalo ga tambahin teks goblok yatim!__")
   
   if mode == "text_on_cmd":
     usrnum = 0
@@ -82,5 +82,5 @@ async def mentionall(event):
         usrnum = 0
         usrtxt = ""
         
-print(">> BOT STARTED <<")
+print(">> BOT AKTIF YE NGENTOT <<")
 client.run_until_disconnected()
